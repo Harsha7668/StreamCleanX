d77c9acff6cb7965d4a6232d59c02b48cd64af9c
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2025-05-25 13:29:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8163251890:AAGpC1g4Hi_PCRd352LwmuK-6aS9DZzcwIA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8163251890:AAHQ6xqQJ-NxEV6wsEdqgNEuRYGDvUMLidQ")
 CAPTION = os.environ.get("CAPTION", "")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
@@ -17,6 +17,6 @@ ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 SUNRISES_PIC= "https://envs.sh/lHg.jpg"  # Replace with your Telegraph link
 LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-PORT = int(os.environ.get("PORT", "8081")) #8080
+PORT = int(os.environ.get("PORT", "8080")) #8080
 
 
